5df751de88ad5a42eff2f9bff5e3998d8edb851e
UhGooma <53350163+UhGoomba@users.noreply.github.com>
2024-05-31 17:30:31 -0400

    it compiles separate main file

--- OGLRenderer/src/main.cpp
+++ OGLRenderer/src/main.cpp
@@ -1,29 +1,6 @@
-#ifdef _DEBUG
-// CRT Memory Leak detection
-	#define _CRTDBG_MAP_ALLOC  
-	#include <stdlib.h>  
-	#include <crtdbg.h>
-#endif
-#include "Engine.h"
+#include <iostream>
 
-#include "Demos/DemoCrytekSponza.h"
-
-/***********************************************************************************/
-int main() {
-#ifdef _DEBUG
-	// Detects memory leaks upon program exit
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-
-	Engine engine("Data/config.xml");
-
-	const auto scene = std::make_shared<DemoCrytekSponza>();
-	scene->Init("Sponza");
-
-	engine.AddScene(std::static_pointer_cast<SceneBase, DemoCrytekSponza>(scene));
-	engine.SetActiveScene("Sponza");
-
-	engine.Execute();
-
-	return 0;
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello World" << std::endl;
 }
